9e9fd273b41fcf73fa83c3070bbcbe629c72f7c3
Alejandro <10854218+gonz92alex@users.noreply.github.com>
2025-12-06 12:10:53 +0100

    ADD first test

---
+++ js/leer.js
@@ -0,0 +1,18 @@
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+  document.getElementById('result').innerText = `Code matched = ${decodedText}`, decodedResult
+}
+
+let config = {
+  fps: 10,
+  qrbox: {width: 150, height: 150},
+  rememberLastUsedCamera: true,
+  // Only support camera scan type.
+  supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+  showTorchButtonIfSupported: true
+};
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader", config, verbose= false);
+html5QrcodeScanner.render(onScanSuccess);
